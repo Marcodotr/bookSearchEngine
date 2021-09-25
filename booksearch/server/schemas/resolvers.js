cd858b83f4732669b2120bbdc75eaded4b9eca5b
unknown <marcodotr@gmail.coom>
2021-09-24 21:28:59 -0500

    server.js

--- booksearch/server/schemas/resolvers.js
+++ booksearch/server/schemas/resolvers.js
@@ -12,23 +12,28 @@ const resolvers = {
         
     },
     Mutation: {
-        createUser: async (parent, args) => {
-            const user = await User.create(args)
-            return user;
-        },
-        login: async (parent, args) => {
-            const user = await User.findOne(
-                { $or: [{ username: args.username }, 
-                { email: body.email }]}
-            );
-            
-            const correctPw = await user.isCorrectPassword(args.password)
-
-            return {
-                user,
-                correctPw
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+          },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
             }
-        }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          }
     }
 }
 module.exports = resolvers
